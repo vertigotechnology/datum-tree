79a54def3c1c134d2d5aa12cb27e09635bdfc39e
Ben Lippmeier <benl@ouroborus.net>
2016-04-08 22:38:51 +1000

    Cleanup csv loader using sexp format

--- Datum/Data/Tree/Codec.hs
+++ Datum/Data/Tree/Codec.hs
@@ -3,34 +3,33 @@ module Datum.Data.Tree.Codec
         (decodeCSV)
 where
 import Datum.Data.Tree.Exp
+import Datum.Data.Tree.SExp
 import qualified Data.Csv                       as Csv
 import qualified Data.ByteString.Lazy.Char8     as BS8
 import qualified Data.Vector                    as V
 
 
+-- | Decode a CSV file from a lazy `ByteString`
 decodeCSV :: BS8.ByteString -> Either String (Tree 'O)
 decodeCSV bs
  = do   
+        -- Decode the file into a vector of rows of vectors of fields.
+        -- TODO: slurp header in post processing.
         vvb     <- Csv.decode Csv.NoHeader bs
 
-        let makeRow vfs
-                = let  fs      = map BS8.unpack $ V.toList vfs
-                  in   B (T $ map AText fs) []
+        -- Make a branch for a single row of the file.
+        let makeRow vFields
+                = let  lFields = map BS8.unpack $ V.toList vFields
+                  in   B (T $ map AText lFields) []
 
-        let g   = G Nothing $ map makeRow 
-                $ V.toList vvb
-
+        -- Maximum number of fields in any row.
         let len = V.maximum $ V.map V.length vvb
 
-        let bt  = BT    "row" 
-                        (TT [("col" ++ show n, ATText) | n <- [0.. len - 1]])
-                        []
-
-        return  $ Tree  (B (T []) [g])
-
-                        (BT "root"
-                                (TT [("name", ATText)])
-                                [bt])
-
-
-
+        return
+         $ Tree (branch tuple 
+                        (G Nothing $ map makeRow $ V.toList vvb))
+                (tbranch "root"
+                        (ttuple (telement "name" ttext))
+                        (tbranch "row" 
+                                (TT [ telement ("col" ++ show n) ttext 
+                                    | n <- [0.. len - 1]])))

778f27a515dc0a983d9f330e5da3cd8f4697308b
Ben Lippmeier <benl@ouroborus.net>
2016-07-06 19:39:51 +1000

    Cleanup matryo encoder

--- Datum/Data/Tree/Codec/Matryo.hs
+++ Datum/Data/Tree/Codec/Matryo.hs
@@ -4,8 +4,9 @@ module Datum.Data.Tree.Codec.Matryo
 where
 import Datum.Data.Tree.Exp
 import Data.Monoid
-import qualified Data.ByteString.Lazy.Char8     as BS8
+import Data.ByteString.Builder                  (Builder)
 import qualified Data.ByteString.Builder        as BB
+import qualified Data.ByteString.Lazy.Char8     as BS8
 import qualified Data.List                      as List
 import qualified Data.Repa.Array                as A
 
@@ -15,42 +16,47 @@ encodeMatryo :: Tree 'O -> BS8.ByteString
 encodeMatryo tree'
  = BB.toLazyByteString $ encodeTree 0 tree'
  where
-        -------------------------------
-        encodeTree   i (Tree b bt)
-         =  encodeBranchType i True bt
-         <> encodeBranch     i True b
 
-        -------------------------------
-        -- Encode a branch type. 
-        --  We track the current indent and whether this is the
-        --  first branch type in the tree meta-data.
-        encodeBranchType i bFirst (BT _n tt bts)
-         |  [] <- unboxes bts
-         =  encodeTupleType tt
-         <> BB.string8 "\n"
 
-         | otherwise
-         =  BB.string8 "{ "
-         <> encodeTupleType tt
-         <> BB.string8 "\n"
-         <> encodeBranchTypes (i + 4) (unboxes bts)
-         <> BB.string8 "\n"
-         <> (if bFirst
-                then mempty
-                else BB.string8 (replicate (i + 2) ' '))
-         <> BB.string8 "}\n"
+-- | Encode a tree.
+encodeTree :: Int -> Tree 'O -> Builder
+encodeTree   i (Tree b bt)
+        =  encodeBranchType i True bt
+        <> encodeBranch     i True b
 
-        -------------------------------
-        encodeBranchTypes :: Int -> [BranchType] -> BB.Builder
-        encodeBranchTypes _i []
-         = mempty
 
-        encodeBranchTypes i bts
-         =   BB.string8 (replicate i ' ') <> BB.string8 "[ "
-         <> (encodeBranchTypess i (0 :: Int) bts)
-         <>  BB.string8 (replicate i ' ') <> BB.string8 "]"
+-- | Encode a branch type. 
+--   We track the current indent and whether this is the
+--   first branch type in the tree meta-data.
+encodeBranchType :: Int -> Bool -> BranchType -> Builder
+encodeBranchType i bFirst (BT _n tt bts)
+        |  [] <- unboxes bts
+        =  encodeTupleType tt
+        <> BB.string8 "\n"
 
-        encodeBranchTypess i 0 (b : bs)
+        | otherwise
+        =  BB.string8 "{ "
+        <> encodeTupleType tt
+        <> BB.string8 "\n"
+        <> encodeBranchTypes (i + 4) (unboxes bts)
+        <> BB.string8 "\n"
+        <> (if bFirst
+               then mempty
+               else BB.string8 (replicate (i + 2) ' '))
+        <> BB.string8 "}\n"
+
+
+-- | Encode a list of branch types.
+encodeBranchTypes :: Int -> [BranchType] -> Builder
+encodeBranchTypes _i []
+        = mempty
+
+encodeBranchTypes i0 bts
+ =   BB.string8 (replicate i0 ' ') <> BB.string8 "[ "
+ <> (encodeBranchTypess i0 (0 :: Int) bts)
+ <>  BB.string8 (replicate i0 ' ') <> BB.string8 "]"
+
+ where  encodeBranchTypess i 0 (b : bs)
          =  encodeBranchType i False b
          <> encodeBranchTypess i 1 bs
 
@@ -62,45 +68,53 @@ encodeMatryo tree'
         encodeBranchTypess _ _ []
          = mempty
 
-        -------------------------------
-        encodeTupleType (TT kts)
-         =  BB.char8 '('
-         <> ( mconcat $ List.intersperse (BB.string8 ", ")
-            $ map encodeKeyType $ A.toList kts)
-         <> BB.char8 ')'
 
-        encodeKeyType (Box n :*: Box at)
-         =  BB.string8 (show n)
-         <> BB.string8 ": "
-         <> BB.string8 (show at)
+-- | Encode a tuple type.
+encodeTupleType :: TupleType -> Builder
+encodeTupleType (TT kts)
+        =  BB.char8 '('
+        <> ( mconcat $ List.intersperse (BB.string8 ", ")
+           $ map encodeKeyType $ A.toList kts)
+        <> BB.char8 ')'
 
-        -------------------------------
-        -- Encode a branch. 
-        --  We track the current indent and whether this is the
-        --  first branch type in the tree.
-        encodeBranch i bFirst (B t gs)
-         |  [] <- unboxes gs
-         =  encodeTuple t
-         <> BB.string8 "\n"
 
-         | otherwise
-         =  BB.string8 "{ "
-         <> encodeTuple t
-         <> BB.string8 "\n"
-         <> (mconcat $ map (encodeGroup (i + 4)) $ unboxes gs)
-         <> BB.string8 "\n"
-         <> (if bFirst
-                then mempty
-                else BB.string8 (replicate (i + 2) ' '))
-         <> BB.string8 "}\n"
+-- | Encode a key type.
+encodeKeyType :: (Box Name :*: Box AtomType) -> Builder
+encodeKeyType (Box n :*: Box at)
+        =  BB.string8 (show n)
+        <> BB.string8 ": "
+        <> encodeAtomType at
 
-        -------------------------------
-        encodeGroup  i (G _name bs)
-         =   BB.string8 (replicate i ' ') <> BB.string8 "[ "
-         <> (encodeGroupBranches   i 0 $ unboxes bs)
-         <>  BB.string8 (replicate i ' ') <> BB.string8 "]"
 
-        encodeGroupBranches i (0 :: Int) (b : bs)
+-- | Encode a branch. 
+--   We track the current indent and whether this is the
+--   first branch type in the tree.
+encodeBranch :: Int -> Bool -> Branch -> Builder
+encodeBranch i bFirst (B t gs)
+        |  [] <- unboxes gs
+        =  encodeTuple t
+        <> BB.string8 "\n"
+
+        | otherwise
+        =  BB.string8 "{ "
+        <> encodeTuple t
+        <> BB.string8 "\n"
+        <> (mconcat $ map (encodeGroup (i + 4)) $ unboxes gs)
+        <> BB.string8 "\n"
+        <> (if bFirst
+               then mempty
+               else BB.string8 (replicate (i + 2) ' '))
+        <> BB.string8 "}\n"
+
+
+-- | Encode a branch group.
+encodeGroup :: Int -> Group -> Builder
+encodeGroup  i0 (G _name bs0)
+ =   BB.string8 (replicate i0 ' ') <> BB.string8 "[ "
+ <> (encodeGroupBranches   i0 0 $ unboxes bs0)
+ <>  BB.string8 (replicate i0 ' ') <> BB.string8 "]"
+
+ where  encodeGroupBranches i (0 :: Int) (b : bs)
          =  encodeBranch i False b 
          <> encodeGroupBranches i 1 bs
 
@@ -112,22 +126,40 @@ encodeMatryo tree'
         encodeGroupBranches _ _ []
          = mempty
 
-        -------------------------------
-        encodeTuple  (T as)
-         =  BB.char8 '('
-         <> ( mconcat $ List.intersperse (BB.string8 ", ") 
-            $ map encodeAtom $ unboxes as)
-         <> BB.char8 ')'
 
-        -------------------------------
-        encodeAtom a
-         = case a of
-                AUnit{}      -> BB.string8  "()"
-                ABool    b   -> BB.string8   (show b)
-                AInt     i   -> BB.intDec    i
-                AFloat   f   -> BB.doubleDec f
-                ANat     n   -> BB.intDec    n
-                ADecimal n   -> BB.doubleDec n
-                AText    str -> BB.string8   $ show str
-                ATime    str -> BB.string8   str
+-- | Encode a tuple.
+encodeTuple :: Tuple -> Builder
+encodeTuple  (T as)
+        =  BB.char8 '('
+        <> ( mconcat $ List.intersperse (BB.string8 ", ") 
+           $ map encodeAtom $ unboxes as)
+        <> BB.char8 ')'
+
+
+-- | Encode an atom.
+encodeAtom :: Atom -> Builder
+encodeAtom a
+ = case a of
+        AUnit{}      -> BB.string8  "()"
+        ABool    b   -> BB.string8   (show b)
+        AInt     i   -> BB.intDec    i
+        AFloat   f   -> BB.doubleDec f
+        ANat     n   -> BB.intDec    n
+        ADecimal n   -> BB.doubleDec n
+        AText    str -> BB.string8   $ show str
+        ATime    str -> BB.string8   str
+
+
+-- | Encode an atom type.
+encodeAtomType :: AtomType -> Builder
+encodeAtomType at
+ = case at of
+        ATUnit          -> BB.string8 "()"
+        ATBool          -> BB.string8 "Bool"
+        ATInt           -> BB.string8 "Int"
+        ATFloat         -> BB.string8 "Float"
+        ATNat           -> BB.string8 "Nat"
+        ATDecimal       -> BB.string8 "Decimal"
+        ATText          -> BB.string8 "Text"
+        ATTime          -> BB.string8 "Time"
 
